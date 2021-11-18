9ef49a884ae0deb6fe446fb28ae1019beeed53bc
YasmeeenBn <58303603+YasmeeenBn@users.noreply.github.com>
2021-11-18 23:17:39 +0100

    .env

--- src/controllers/scappong.js
+++ src/controllers/scappong.js
@@ -4,9 +4,6 @@ const Organization = require('../models/organization');
 const Country = require('../models/country');
 const NewsBreakdown = require('../models/newsBreakdown');
 
-
-
-
 async function getNews() {
     var url = "https://news.google.com/topics/CAAqIQgKIhtDQkFTRGdvSUwyMHZNRFIzWjJnU0FtWnlLQUFQAQ?hl=fr&gl=MA&ceid=MA%3Afr";
     // document.querySelector("#gb > div.gb_Cc.gb_Ac.gb_la.gb_Fc.gb_Hc.gb_Ec > div.gb_Ic > div > c-wiz > div > div:nth-child(5) > div:nth-child(4)")
@@ -15,14 +12,11 @@ async function getNews() {
     page.setDefaultNavigationTimeout(0);
     await page.goto(url, { waitUntil: 'networkidle2' });
     let newsUrls = await page.evaluate(() => {
-        var articlesurls = []
+        var articles = []
         // var maroc = document.querySelector("#gb > div.gb_Cc.gb_Ac.gb_la.gb_Fc.gb_Hc.gb_Ec > div.gb_Ic > div > c-wiz > div > div:nth-child(5) > div:nth-child(4)");
         // var inter = document.querySelector("#gb > div.gb_Cc.gb_Ac.gb_la.gb_Fc.gb_Hc.gb_Ec > div.gb_Ic > div > c-wiz > div > div:nth-child(5) > div:nth-child(5)");
         var articles = document.querySelectorAll('.VDXfz');
         console.log(articles);
-
-
-
     });
     await browser.close();
     // forward google news urls to target urls
@@ -32,20 +26,6 @@ async function getNews() {
         newsUrls[i].article_url = targetUrl[0];
     }
     return newsUrls
-}
-
-async function getSubOrgsInfo() {
-    var subOrganizations = await Organization.find({head_office_id:{$nin:[null]}});
-    var infos = []
-    for (let i = 0; i < subOrganizations.length; i++) {
-        var info = {
-            "orgId": subOrganizations[i]._id,
-            "orgName": subOrganizations[i].name,
-            "countryId": subOrganizations[i].country,
-        }
-        infos.push(info)
-    }
-    return infos
 }
 
 
@@ -76,37 +56,4 @@ function sleep(ms) {
     });
 }
 
-
-exports.addOrgArticles = async(req, res, next) => {
-    const subOrgInfos = await getSubOrgsInfo();
-
-    var offsetVal = await NewsBreakdown.find({});
-    var offset = offsetVal[0].offset;
-    var errorIndex = offset;
-    try {
-        for (let i = offset; i < subOrgInfos.length; i++) {
-            errorIndex++;
-            try {
-                await saveOrgArticles(subOrgInfos[i]);
-
-                await sleep(60000);
-            } catch (error) {
-
-                await NewsBreakdown.updateOne({ "_id": "61015409cc93eb146c668f6d" }, { $set: { interrupted: true, error_description: error, organization: subOrgInfos[i], offset: i } })
-                return null;
-            }
-            await NewsBreakdown.updateOne({ "_id": "61015409cc93eb146c668f6d" }, { $set: { interrupted: true, error_description: "", organization: {}, offset: i } });
-
-        }
-
-        await NewsBreakdown.updateOne({ "_id": "61015409cc93eb146c668f6d" }, { $set: { interrupted: false, error_description: "", organization: {}, offset: 0 } });
-
-        // res.status(200).json("ok");
-    } catch (error) {
-        console.error(error);
-        await NewsBreakdown.updateOne({ "_id": "61015409cc93eb146c668f6d" }, { $set: { interrupted: true, error_description: error, organization: {}, offset: errorIndex } });
-        // res.status(404).json(error);
-
-    }
-}
 
