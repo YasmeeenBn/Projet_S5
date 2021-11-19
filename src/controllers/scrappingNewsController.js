b21070f1f8a8678e43a897d2a88bfd526cc9aba6
issambouhraoua <issambouhraoua1999@gmail.com>
2021-11-20 00:23:34 +0200

    main

--- src/controllers/scrappingNewsController.js
+++ src/controllers/scrappingNewsController.js
@@ -1,6 +1,8 @@
 const puppeteer = require('puppeteer');
 const News = require('../models/news');
 
+
+
 async function getNews() {
     var url = "https://news.google.com/topics/CAAqIQgKIhtDQkFTRGdvSUwyMHZNRFIzWjJnU0FtWnlLQUFQAQ?hl=fr&gl=MA&ceid=MA%3Afr";
     // document.querySelector("#gb > div.gb_Cc.gb_Ac.gb_la.gb_Fc.gb_Hc.gb_Ec > div.gb_Ic > div > c-wiz > div > div:nth-child(5) > div:nth-child(4)")
@@ -11,9 +13,36 @@ async function getNews() {
     let newsUrls = await page.evaluate(() => {
         var articles = []
         // var maroc = document.querySelector("#gb > div.gb_Cc.gb_Ac.gb_la.gb_Fc.gb_Hc.gb_Ec > div.gb_Ic > div > c-wiz > div > div:nth-child(5) > div:nth-child(4)");
-        // var inter = document.querySelector("#gb > div.gb_Cc.gb_Ac.gb_la.gb_Fc.gb_Hc.gb_Ec > div.gb_Ic > div > c-wiz > div > div:nth-child(5) > div:nth-child(5)");
-        var articles = document.querySelectorAll('.VDXfz');
+         // var inter = document.querySelector("#gb > div.gb_Cc.gb_Ac.gb_la.gb_Fc.gb_Hc.gb_Ec > div.gb_Ic > div > c-wiz > div > div:nth-child(5) > div:nth-child(5)");
+        
+         var articles = document.querySelectorAll('.VDXfz');
+         var article_url = articles.href;
         console.log(articles);
+    });
+    await browser.close();
+    // forward google news urls to target urls
+
+    for (var i = 0; i < newsUrls.length; i++) {
+        var targetUrl = await forwardUrl(newsUrls[i].article_url);
+        newsUrls[i].article_url = targetUrl[0];
+    }
+    return newsUrls
+}
+
+
+async function getArticleNews() {
+    
+    //lien de l'article
+    var article_url = "https://news.google.com/topics/CAAqIQgKIhtDQkFTRGdvSUwyMHZNRFIzWjJnU0FtWnlLQUFQAQ?hl=https://news.google.com/articles/CBMiSmh0dHBzOi8vd3d3LmNubi5jb20vMjAyMS8xMS8xOS91cy9reWxlLXJpdHRlbmhvdXNlLXRyaWFsLWZyaWRheS9pbmRleC5odG1s0gFOaHR0cHM6Ly9hbXAuY25uLmNvbS9jbm4vMjAyMS8xMS8xOS91cy9reWxlLXJpdHRlbmhvdXNlLXRyaWFsLWZyaWRheS9pbmRleC5odG1s?hl=en-US&gl=US&ceid=US%3Aen&gl=MA&ceid=MA%3Afr";
+    // document.querySelector("#gb > div.gb_Cc.gb_Ac.gb_la.gb_Fc.gb_Hc.gb_Ec > div.gb_Ic > div > c-wiz > div > div:nth-child(5) > div:nth-child(4)")
+    var browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+    var page = (await browser.pages())[0];
+    page.setDefaultNavigationTimeout(0);
+    await page.goto(article_url, { waitUntil: 'networkidle2' });
+    let newsUrls = await page.evaluate(() => {
+        //container de l'article dont on doit acceder au titre etc ..               
+        var article = document.querySelector('.l-container');
+        console.log(article);
     });
     await browser.close();
     // forward google news urls to target urls
